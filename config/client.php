8fdaa328ce0a5eed173a73bec6a1f309086b0563
Mike Burg <mike@burg.me>
2019-01-07 11:18:41 -0800

    Added PersonnelEmail config.

--- config/client.php
+++ config/client.php
@@ -35,6 +35,7 @@ return [
     'SiteTitle'            => 'Black Rock Rangers Secret Clubhouse',
     'AdminEmail'           => env('RANGER_CLUBHOUSE_EMAIL_ADMIN', 'ranger-tech-ninjas@burningman.org'),
     'GeneralSupportEmail'  => env('RANGER_CLUBHOUSE_EMAIL_SUPPORT', 'rangers@burningman.org'),
+    'PersonnelEmail'       => env('RANGER_CLUBHOUSE_EMAIL_PERSONNEL', 'ranger-personnel@burningman.org'),
     'VCEmail'              => env('RANGER_CLUBHOUSE_EMAIL_VC', 'ranger-vc-list@burningman.org'),
     'SignupUrl'            => 'http://jousting-at-windmills.org/clubhouse/',
 
