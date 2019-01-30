035fe3b7152fbf1d30b1e2905b100b9828093b35
Mike Burg <mike@burg.me>
2019-01-30 15:41:50 -0800

    rename action_logs table migration in prep for full database schema & testing.

--- database/migrations/2019_01_01_102126_create_action_logs_table.php
+++ database/migrations/2019_01_01_102126_create_action_logs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ActionLog extends Migration
+class CreateActionLogsTable extends Migration
 {
     /**
      * Run the migrations.
