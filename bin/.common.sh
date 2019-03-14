a211842942637cef85b0600b9a2087cca7249de2
Wilfredo Sánchez Vega <wsanchez@apple.com>
2019-03-14 11:24:47 -0700

    Run unit tests

--- bin/.common.sh
+++ bin/.common.sh
@@ -6,4 +6,7 @@ image_repo="ranger-clubhouse-api";
  image_tag="dev";
 image_name="${image_repo}:${image_tag}";
 
+build_image_name="${image_repo}_build";
+  dev_image_name="${image_repo}_dev";
+
 container_name="ranger-clubhouse-api";
