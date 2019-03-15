f814d53b1e0fa5befc21c4a4e0779c1cfb044709
Wilfredo Sánchez Vega <wsanchez@apple.com>
2019-03-14 17:10:30 -0700

    Start DB container

--- bin/.common.sh
+++ bin/.common.sh
@@ -11,4 +11,7 @@ composer_image_name="${image_repo}_composer";
    build_image_name="${image_repo}_build";
      dev_image_name="${image_repo}_dev";
 
+ db_image_name="mysql/mysql-server:5.6";
+api_image_name="${image_name}";
+
 container_name="ranger-clubhouse-api";
