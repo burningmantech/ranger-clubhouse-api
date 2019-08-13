d5648862bb62997f8c81ed161b2a5e4682358fef
mikeburg <mike@burg.me>
2019-08-12 21:07:06 -0700

    Revert "Don't count hours for non credit earning shifts."

--- app/Http/Controllers/PersonScheduleController.php
+++ app/Http/Controllers/PersonScheduleController.php
@@ -44,6 +44,11 @@ class PersonScheduleController extends ApiController
 
         $rows = Schedule::findForQuery($query);
 
+        if (!$rows->isEmpty()) {
+            // Warm the position credit cache.
+            PositionCredit::warmYearCache($query['year'], array_unique($rows->pluck('position_id')->toArray()));
+        }
+
         return $this->success($rows, null, 'schedules');
     }
 
@@ -475,6 +480,11 @@ class PersonScheduleController extends ApiController
             'year'      => $year,
             'remaining' => true
         ]);
+
+        if (!$rows->isEmpty()) {
+            // Warm the position credit cache.
+            PositionCredit::warmYearCache($year, array_unique($rows->pluck('position_id')->toArray()));
+        }
 
         $time = 0;
         $credits = 0.0;
