26b62ca393f2fc7a96d1b1f5dfb4574ffffb1e5d
Mike Burg <mike@burg.me>
2019-10-25 17:54:40 -0700

    Clubhouse 1 Log port.

---
+++ app/Http/Controllers/Clubhouse1LogController.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * Clubhouse 1 Log controller - provided purely for historical purposes.
+ *
+ */
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\Clubhouse1Log;
+use App\Models\Person;
+use App\Models\Role;
+
+class Clubhouse1LogController extends ApiController
+{
+    /**
+     * Retrieve the action log
+     */
+
+     public function index()
+     {
+         if (!$this->userHasRole(Role::ADMIN)) {
+             $this->notPermitted('Only Admins may view the Clubhouse 1 Log');
+         }
+
+         $params = request()->validate([
+             'sort'       => 'sometimes|string',
+             'events'     => 'sometimes|string',
+             'event_text' => 'sometimes|string',
+
+             'start_time'  => 'sometimes|date',
+             'end_time'    => 'sometimes|date',
+
+             'page'       => 'sometimes|integer',
+             'page_size'  => 'sometimes|integer',
+
+             'person'     => 'sometimes|string',
+        ]);
+
+        if (isset($params['person'])) {
+            $callsign = $params['person'];
+            if (is_numeric($callsign)) {
+                $params['person_id'] = (int) $callsign;
+            } else {
+                $person = Person::findByCallsign($callsign);
+                if (!$person) {
+                    return response()->json([ 'error' => "Person $callsign was not found."]);
+                }
+
+                $params['person_id'] = $person->id;
+            }
+        }
+
+        return response()->json(Clubhouse1Log::findForQuery($params));
+    }
+}
