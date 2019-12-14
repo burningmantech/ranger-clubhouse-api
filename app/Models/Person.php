4921f18fb28aae86812048b5e2b298a8cef6352b
Mike Burg <mike@burg.me>
2019-12-14 12:16:21 -0800

    Added vintage and years rangered fields to People By Status Change report.

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -598,6 +598,9 @@ class Person extends ApiModel implements JWTSubject, AuthenticatableContract, Au
             return !preg_match('/(^(testing|lam #|temp \d+))|\(test\)/i', $r->callsign);
         };
 
+        $yearsRangered = DB::raw('(SELECT COUNT(DISTINCT(YEAR(on_duty))) FROM timesheet WHERE person_id=person.id AND position_id NOT IN (1, 13, 29, 30)) AS years');
+        $lastYear = DB::raw('(SELECT YEAR(on_duty) FROM timesheet WHERE person_id=person.id ORDER BY on_duty DESC LIMIT 1) AS last_year');
+
         // Inactive means that you have not rangered in any of the last 3 events
         // but you have rangered in at least one of the last 5 events
         $inactives = Person::select(
@@ -605,7 +608,9 @@ class Person extends ApiModel implements JWTSubject, AuthenticatableContract, Au
             'callsign',
             'status',
             'email',
-            DB::raw('(SELECT YEAR(on_duty) FROM timesheet WHERE person_id=person.id ORDER BY on_duty DESC LIMIT 1) AS last_year')
+            'vintage',
+            $lastYear,
+            $yearsRangered
         )->where('status', 'active')
                 ->whereRaw('person.id NOT IN (SELECT person_id FROM timesheet WHERE YEAR(on_duty) BETWEEN ? AND ?)', [ $year - 3, $year ])
                 ->whereRaw('person.id IN (SELECT person_id FROM timesheet WHERE YEAR(on_duty) BETWEEN ? AND ?)', [ $year - 5, $year - 4])
@@ -619,7 +624,9 @@ class Person extends ApiModel implements JWTSubject, AuthenticatableContract, Au
             'callsign',
             'status',
             'email',
-            DB::raw('(SELECT YEAR(on_duty) FROM timesheet WHERE person_id=person.id ORDER BY on_duty DESC LIMIT 1) AS last_year')
+            'vintage',
+            $lastYear,
+            $yearsRangered
         )->whereIn('status', [ Person::ACTIVE, Person::INACTIVE, Person::INACTIVE_EXTENSION ])
                 ->whereRaw('person.id NOT IN (SELECT person_id FROM timesheet WHERE YEAR(on_duty) BETWEEN ? AND ?)', [ $year - 5, $year ])
                 ->orderBy('callsign')
@@ -634,6 +641,7 @@ class Person extends ApiModel implements JWTSubject, AuthenticatableContract, Au
                     'callsign',
                     'status',
                     'email',
+                    'vintage',
                     DB::raw('YEAR(MAX(on_duty)) AS last_year'),
                     DB::raw('count(distinct(YEAR(on_duty))) as years')
                 )
@@ -641,7 +649,7 @@ class Person extends ApiModel implements JWTSubject, AuthenticatableContract, Au
                 ->whereIn('status', [ Person::ACTIVE, Person::INACTIVE ])
                 ->whereNotIn('position_id', [ Position::ALPHA, Position::TRAINING ])
                 ->where('vintage', false)
-                ->groupBy([ 'person_id', 'callsign', 'status', 'email' ])
+                ->groupBy([ 'person_id', 'callsign', 'status', 'email', 'vintage' ])
                 ->havingRaw('count(distinct(YEAR(on_duty))) >= 10')
                 ->orderBy('callsign')
                 ->get()
@@ -653,7 +661,9 @@ class Person extends ApiModel implements JWTSubject, AuthenticatableContract, Au
             'callsign',
             'status',
             'email',
-            DB::raw('(SELECT YEAR(on_duty) FROM timesheet WHERE person_id=person.id ORDER BY on_duty DESC LIMIT 1) AS last_year')
+            'vintage',
+            $lastYear,
+            $yearsRangered
         )->whereIn('status', [ Person::INACTIVE, Person::INACTIVE_EXTENSION, Person::RETIRED ])
                 ->whereRaw('person.id IN (SELECT person_id FROM timesheet WHERE YEAR(on_duty) BETWEEN ? AND ?)', [ $year - 3, $year])
                 ->orderBy('callsign')
