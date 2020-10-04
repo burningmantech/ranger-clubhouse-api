d19346ec1195bf6fd41f21aab4cd570098a60bae
Victor Asteinza <civrot@users.noreply.github.com>
2020-10-04 16:58:58 -0700

    Factor out repair into the new check classes
    - Refactor out the repair method into the newly formed check classes for
      each type of check.
    - Rename the repair names so that they are JSON friendly.

--- app/Lib/PositionSanityCheck.php
+++ app/Lib/PositionSanityCheck.php
@@ -42,89 +42,18 @@ class PositionSanityCheck
     /**
      * Repair position / role problems
      *
-     * @param string $repair - the name of the thing to repair 'green-dot', 'management-role', 'shiny-penny'
+     * @param string $repair - the name of the thing to repair 'green_dot', 'management_role', 'shiny_penny'
      * @param array $peopleIds - list of person ids to repair. Ids are assumed to exist.
      * @return array
      */
 
     public static function repair(string $repair, array $peopleIds): array
     {
-        $results = [];
-
-        switch ($repair) {
-            case 'green-dot':
-                foreach ($peopleIds as $personId) {
-                    $messages = [];
-                    $errors = [];
-
-                    $hasDirt = PersonPosition::havePosition($personId, Position::DIRT_GREEN_DOT);
-                    $hasSanctuary = PersonPosition::havePosition($personId, Position::SANCTUARY);
-                    $hasGPGD = PersonPosition::havePosition($personId, Position::GERLACH_PATROL_GREEN_DOT);
-
-                    if (!$hasDirt && !$hasSanctuary) {
-                        $errors[] = 'not a Green Dot';
-                    } else {
-                        $positionIds = [];
-
-                        if (!$hasDirt) {
-                            $positionIds[] = Position::DIRT_GREEN_DOT;
-                            $messages[] = 'added Dirt - Green Dot';
-                        }
-
-                        if (!$hasSanctuary) {
-                            $positionIds[] = Position::SANCTUARY;
-                            $messages[] = 'added Sanctuary';
-                        }
-                        if (!$hasGPGD) {
-                            $positionIds[] = Position::GERLACH_PATROL_GREEN_DOT;
-                            $messages[] = 'added Gerlach Patrol - Green Dot';
-                        }
-                        PersonPosition::addIdsToPerson($personId, $positionIds, 'position sanity checker repair');
-                    }
-
-                    $result = [
-                        'id' => $personId,
-                        'messages' => $messages
-                    ];
-
-                    if (!empty($errors)) {
-                        $result['errors'] = $errors;
-                    }
-
-                    $results[] = $result;
-                }
-                return $results;
-
-            case 'management-role':
-                foreach ($peopleIds as $personId) {
-                    PersonRole::addIdsToPerson($personId, [Role::MANAGE], 'position sanity checker repair');
-                    $results[] = ['id' => $personId];
-                }
-                return $results;
-
-            case 'shiny-penny':
-                $year = current_year();
-
-                foreach ($peopleIds as $personId) {
-                    $hasPenny = PersonPosition::havePosition($personId, Position::DIRT_SHINY_PENNY);
-                    $isPenny = PersonMentor::retrieveYearPassed($personId) == $year;
-
-                    if ($hasPenny && !$isPenny) {
-                        PersonPosition::removeIdsFromPerson($personId, [Position::DIRT_SHINY_PENNY], 'position sanity checker repair');
-                        $message = 'not a Shiny Penny, position removed';
-                    } elseif (!$hasPenny && $isPenny) {
-                        PersonPosition::addIdsToPerson($personId, [Position::DIRT_SHINY_PENNY], 'position sanity checker repair');
-                        $message = 'is a Shiny Penny, position added';
-                    } else {
-                        $message = 'Shiny Penny already has position. no repair needed.';
-                    }
-                    $results[] = ['id' => $personId, 'messages' => [$message]];
-                }
-                return $results;
-
-            default:
-                throw new InvalidArgumentException("Unknown repair action [$repair]");
+        if (!array_key_exists($repair, self::CHECKERS)) {
+            throw new InvalidArgumentException("Unknown repair action [$repair]");
         }
+
+        return self::CHECKERS[$repair]::repair($peopleIds);
     }
 
 }
