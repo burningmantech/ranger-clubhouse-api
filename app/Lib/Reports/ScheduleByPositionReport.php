5ae71caefb4e335b720806a253d3a8f6a7cd4273
Mike Burg <mike@burg.me>
2020-12-01 17:35:51 -0800

    forgot to add files. d'oh!

--- app/Lib/Reports/ScheduleByPositionReport.php
+++ app/Lib/Reports/ScheduleByPositionReport.php
@@ -4,7 +4,61 @@
 namespace App\Lib\Reports;
 
 
+use App\Models\Slot;
+use Illuminate\Support\Collection;
+
 class ScheduleByPositionReport
 {
+    /**
+     * Report on all scheduled sign up by position for a given year
+     *
+     * @param int $year
+     * @return Collection
+     */
 
+    public static function execute(int $year)
+    {
+        $rows = Slot::select('slot.*')
+            ->join('position', 'position.id', 'slot.position_id')
+            ->whereYear('begins', $year)
+            ->where('slot.active', true)
+            ->with(['position:id,title,active', 'person_slot.person:id,callsign,status'])
+            ->orderBy('position.title')
+            ->orderBy('slot.begins')
+            ->get()
+            ->groupBy('position_id');
+
+
+        return $rows->map(function ($p) {
+            $slot = $p[0];
+            $position = $slot->position;
+
+            return [
+                'id' => $position->id,
+                'title' => $position->title,
+                'active' => $position->active,
+                'slots' => $p->map(function ($slot) {
+                    $signups = $slot->person_slot->sort(function ($a, $b) {
+                        $aCallsign = $a->person ? $a->person->callsign : "Person #{$a->person_id}";
+                        $bCallsign = $b->person ? $b->person->callsign : "Person #{$b->person_id}";
+                        return strcasecmp($aCallsign, $bCallsign);
+                    })->values();
+
+                    return [
+                        'begins' => (string)$slot->begins,
+                        'ends' => (string)$slot->ends,
+                        'description' => (string)$slot->description,
+                        'max' => $slot->max,
+                        'sign_ups' => $signups->map(function ($row) {
+                            $person = $row->person;
+                            return [
+                                'id' => $row->person_id,
+                                'callsign' => $person ? $person->callsign : "Person #{$row->person_id}"
+                            ];
+                        })
+                    ];
+                })->values()
+            ];
+        })->values();
+    }
 }
--- app/Lib/Reports/ShiftSignupsReport.php
+++ app/Lib/Reports/ShiftSignupsReport.php
@@ -1,10 +1,71 @@
 <?php
 
-
 namespace App\Lib\Reports;
 
+use App\Models\Slot;
 
 class ShiftSignupsReport
 {
+    /*
+     * Shift Signup report
+     */
+
+    public static function execute(int $year)
+    {
+        $rows = Slot::select(
+            'slot.id',
+            'begins',
+            'ends',
+            'description',
+            'signed_up',
+            'max',
+            'position_id'
+        )->whereYear('begins', $year)
+            ->with([ 'position:id,title'])
+            ->orderBy('begins')
+            ->get();
+
+        $rowsByPositions = $rows->groupBy('position_id');
+
+        $positions = [];
+        foreach ($rowsByPositions as $positionId => $shifts) {
+            $totalMax = 0;
+            $totalSignedUp = 0;
+            $emptyShifts = 0;
+            foreach ($shifts as $shift) {
+                $totalMax += $shift->max;
+                $totalSignedUp += $shift->signed_up;
+                if ($shift->signed_up == 0) {
+                    $emptyShifts++;
+                }
+            }
+
+            $positions[] = [
+                'id'              => $positionId,
+                'title'           => $shifts[0]->position->title,
+                'total_signed_up' => $totalSignedUp,
+                'total_max'       => $totalMax,
+                'total_empty'     => $emptyShifts,
+                'full_percentage' => ($totalMax > 0 ? floor(($totalSignedUp * 100) / $totalMax) : 0),
+                'shifts'          => $shifts->map(function ($row) {
+                    return [
+                        'id'          => $row->id,
+                        'begins'      => (string) $row->begins,
+                        'ends'        => (string) $row->ends,
+                        'description' => $row->description,
+                        'signed_up'   => $row->signed_up,
+                        'max'         => $row->max,
+                        'full_percentage' => ($row->max > 0 ? floor(($row->signed_up * 100)/ $row->max) : 0)
+                    ];
+                })
+            ];
+        }
+
+        usort($positions, function ($a, $b) {
+            return strcasecmp($a['title'], $b['title']);
+        });
+
+        return $positions;
+    }
 
 }
