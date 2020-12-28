7eed8af4b073d5246d2296de3767f02bfd40be0d
Mike Burg <mike@burg.me>
2020-12-27 20:18:12 -0800

    Switched to AWS ELB headers for ip detection.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,8 +18,8 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
 //        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\TrustProxies::class,
         \Monicahq\Cloudflare\Http\Middleware\TrustProxies::class,
+        \App\Http\Middleware\TrustProxies::class,
         \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\RequestLogger::class
     ];
