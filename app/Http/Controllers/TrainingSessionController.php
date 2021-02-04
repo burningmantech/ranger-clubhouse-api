cd594e819bcbfa3f9cd4c3efed00c8626f34fbb1
Mike Burg <mike@burg.me>
2021-02-03 17:10:53 -0800

    Allow a trainee note creator to update or delete their note.

--- app/Http/Controllers/TrainingSessionController.php
+++ app/Http/Controllers/TrainingSessionController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\ApiController;
-use App\Models\PersonIntakeNote;
-use Illuminate\Http\Request;
 
 use App\Models\PersonSlot;
 use App\Models\TraineeStatus;
@@ -13,27 +11,35 @@ use App\Models\Training;
 use App\Models\TrainingSession;
 use App\Models\TraineeNote;
 
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Http\JsonResponse;
 
 class TrainingSessionController extends ApiController
 {
-    /*
+    /**
      * Retrieve the session, students and teachers for a given session.
+     *
+     * @param TrainingSession $training_session
+     * @return JsonResponse
+     * @throws AuthorizationException
      */
 
-    public function show($id)
+    public function show(TrainingSession $training_session)
     {
-        $session = TrainingSession::findOrFail($id);
-        $this->authorize('show', $session);
+        $this->authorize('show', $training_session);
 
         return response()->json([
-            'slot' => $session,
-            'students' => $session->retrieveStudents(),
-            'trainers' => $session->retrieveTrainers(),
+            'slot' => $training_session,
+            'students' => $training_session->retrieveStudents(),
+            'trainers' => $training_session->retrieveTrainers(),
         ]);
     }
 
-    /*
-     * Retrieve all the training sessions for a given training.
+    /**
+     *  Retrieve all the training sessions for a given training (position) & year.
+     *
+     * @return JsonResponse
+     * @throws AuthorizationException
      */
 
     public function sessions()
@@ -61,14 +67,17 @@ class TrainingSessionController extends ApiController
         return response()->json(['sessions' => $info]);
     }
 
-    /*
+    /**
      * Score a student
+     *
+     * @param TrainingSession $training_session
+     * @return JsonResponse
+     * @throws AuthorizationException
      */
 
-    public function scoreStudent($slotId)
+    public function scoreStudent(TrainingSession $training_session)
     {
-        $session = TrainingSession::findOrFail($slotId);
-        $this->authorize('score', $session);
+        $this->authorize('score', $training_session);
 
         $params = request()->validate([
             'id' => 'required|integer',
@@ -80,13 +89,15 @@ class TrainingSessionController extends ApiController
 
         $personId = $params['id'];
 
+        $slotId = $training_session->id;
+
         if (!PersonSlot::haveSlot($personId, $slotId)) {
             return $this->restError('Person is not signed up for the slot');
         }
 
         $traineeStatus = TraineeStatus::firstOrNewForSession($personId, $slotId);
         $traineeStatus->rank = $params['rank'];
-        if (!$session->isArt() && isset($params['feedback_delivered'])) {
+        if (!$training_session->isArt() && isset($params['feedback_delivered'])) {
             $traineeStatus->feedback_delivered = $params['feedback_delivered'];
 
         }
@@ -104,25 +115,27 @@ class TrainingSessionController extends ApiController
         }
 
         if ($rankUpdated) {
-            TraineeNote::record($personId, $session->id, "rank change [" . ($oldRank ?? 'no rank') . "] -> [" . ($traineeStatus->rank ?? 'no rank') . "]", true);
+            TraineeNote::record($personId, $slotId, "rank change [" . ($oldRank ?? 'no rank') . "] -> [" . ($traineeStatus->rank ?? 'no rank') . "]", true);
         }
 
         $note = trim($params['note'] ?? '');
         if (!empty($note)) {
-            TraineeNote::record($personId, $session->id, $note);
+            TraineeNote::record($personId, $slotId, $note);
         }
 
-        return response()->json(['students' => $session->retrieveStudents()]);
+        return response()->json(['students' => $training_session->retrieveStudents()]);
     }
 
-    /*
+    /**
      * Mark trainers as attended, or not.
+     *
+     * @param TrainingSession $training_session
+     * @return JsonResponse
+     * @throws AuthorizationException
      */
-
-    public function trainerStatus($id)
+    public function trainerStatus(TrainingSession $training_session)
     {
-        $session = TrainingSession::findOrFail($id);
-        $this->authorize('trainerStatus', $session);
+        $this->authorize('trainerStatus', $training_session);
 
         $params = request()->validate([
             'trainers.*.id' => 'required|integer',
@@ -133,23 +146,25 @@ class TrainingSessionController extends ApiController
         foreach ($params['trainers'] as $trainer) {
             $personId = $trainer['id'];
 
-            $trainerStatus = TrainerStatus::firstOrNewForSession($session->id, $personId);
+            $trainerStatus = TrainerStatus::firstOrNewForSession($training_session->id, $personId);
             $trainerStatus->status = $trainer['status'];
             $trainerStatus->trainer_slot_id = $trainer['trainer_slot_id'];
             $trainerStatus->save();
         }
 
-        return response()->json(['trainers' => $session->retrieveTrainers()]);
+        return response()->json(['trainers' => $training_session->retrieveTrainers()]);
     }
 
     /**
-     * Retrieve the trainers
+     * Find the trainers for a given session.
+     *
+     * @param TrainingSession $training_session
+     * @return JsonResponse
      */
 
-    public function trainers($id)
+    public function trainers(TrainingSession $training_session)
     {
-        $session = TrainingSession::findOrFail($id);
-        $trainerGroups = $session->retrieveTrainers();
+        $trainerGroups = $training_session->retrieveTrainers();
 
         $trainers = [];
         foreach ($trainerGroups as $group) {
@@ -166,5 +181,43 @@ class TrainingSessionController extends ApiController
         });
 
         return response()->json(['trainers' => $trainers]);
+    }
+
+    /**
+     * Update a trainee note. Only allowed by the note's creator.
+     *
+     * @param TraineeNote $trainee_note
+     * @return JsonResponse
+     * @throws AuthorizationException
+     */
+
+    public function updateNote(TraineeNote $trainee_note)
+    {
+        if ($trainee_note->person_source_id != $this->user->id) {
+            $this->notPermitted('Not authorized to update note.');
+        }
+
+        $params = request()->validate(['note' => 'required|string']);
+
+        $trainee_note->note = $params['note'];
+        $trainee_note->saveOrThrow();
+        return $this->success();
+    }
+
+    /**
+     * Delete a trainee note. Only allowed by the note's creator.
+     *
+     * @param TraineeNote $trainee_note
+     * @return JsonResponse
+     * @throws AuthorizationException
+     */
+
+    public function deleteNote(TraineeNote $trainee_note)
+    {
+        if ($trainee_note->person_source_id != $this->user->id) {
+            $this->notPermitted('Not authorized to delete note.');
+        }
+        $trainee_note->delete();
+        return $this->restDeleteSuccess();
     }
 }
