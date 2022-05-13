f20058078ba6591ebaefab7dbdad296649571a9b
mikeburg <mike@burg.me>
2022-05-12 22:16:54 -0700

    Schedule By Callsign report speed up.

--- app/Lib/Reports/ScheduleByCallsignReport.php
+++ app/Lib/Reports/ScheduleByCallsignReport.php
@@ -2,7 +2,8 @@
 
 namespace App\Lib\Reports;
 
-use Illuminate\Support\Facades\DB;
+use App\Models\PersonSlot;
+use App\Models\Slot;
 
 class ScheduleByCallsignReport
 {
@@ -10,61 +11,61 @@ class ScheduleByCallsignReport
      * Schedule By Callsign report
      */
 
-    public static function execute(int $year)
+    public static function execute(int $year): array
     {
-        $peopleGroups = DB::table('slot')
-            ->select('person_slot.*',
-                'person.callsign',
-                'person.status',
-                'slot.id as slot_id',
-                'slot.begins',
-                'slot.ends',
-                'slot.description',
-                'slot.position_id',
-                'position.title as position_title',
-                'position.active as position_active'
-            )
-            ->join('person_slot', 'slot.id', 'person_slot.slot_id')
-            ->join('position', 'position.id', 'slot.position_id')
-            ->join('person', 'person.id', 'person_slot.person_id')
+        $slots = Slot::select('id', 'begins', 'ends', 'description', 'position_id')
             ->whereYear('slot.begins', $year)
-            ->orderBy('callsign')
-            ->orderBy('slot.begins')
-            ->get()
-            ->groupBy('person_id');
+            ->with('position:id,title,active')
+            ->orderBy('begins')
+            ->get();
+
+        if ($slots->isEmpty()) {
+            return [
+                'people' => [],
+                'positions' => [],
+                'slots' => [],
+            ];
+        }
 
         $positions = [];
-        $slots = [];
+        foreach ($slots as $slot) {
+            if (isset($positions[$slot->position_id])) {
+                continue;
+            }
 
-        $people = $peopleGroups->map(function ($signups) use (&$positions, &$slots) {
-            $person = $signups[0];
-            return [
-                'id' => $person->person_id,
-                'callsign' => $person->callsign,
-                'status' => $person->status,
-                'slot_ids' => $signups->map(function ($row) use (&$positions, &$slots) {
-                    $positions[$row->position_id] ??= [
-                        'title' => $row->position_title,
-                        'active' => $row->position_active,
-                    ];
-
-                    $slots[$row->slot_id] ??= [
-                        'id' => $row->slot_id,
-                        'position_id' => $row->position_id,
-                        'begins' => (string)$row->begins,
-                        'ends' => (string)$row->ends,
-                        'description' => $row->description,
-                    ];
-
-                    return $row->slot_id;
-                })->values()
+            $positions[$slot->position_id] = [
+                'id' => $slot->position_id,
+                'title' => $slot->position->title,
+                'active' => $slot->position->active
             ];
-        })->values();
+        }
 
+
+        $signups = PersonSlot::whereIntegerInRaw('slot_id', $slots->pluck('id'))
+            ->with('person:id,callsign,status')
+            ->get();
+
+        $people = [];
+
+        foreach ($signups as $signup) {
+            $person = $signup->person;
+            if (!isset($people[$signup->person_id])) {
+                $people[$signup->person_id] = [
+                    'id' => $person->id,
+                    'callsign' => $person->callsign,
+                    'status' => $person->status,
+                    'slot_ids' => []
+                ];
+            }
+            $people[$signup->person_id]['slot_ids'][] = $signup->slot_id;
+        }
+
+        $people = array_values($people);
+        usort($people, fn($a, $b) => strcasecmp($a['callsign'], $b['callsign']));
         return [
             'people' => $people,
             'positions' => $positions,
-            'slots' => $slots,
+            'slots' => $slots->keyBy('id')->toArray(),
         ];
     }
 }
