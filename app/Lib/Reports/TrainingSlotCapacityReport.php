fe5f64a13efdc935e18fa8e10ee958e2fc71b6e2
mikeburg <mike@burg.me>
2022-12-23 22:43:13 -0800

    Training Capacity Report enhancements (#1332)
    - Return counts using the status of the sign-ups at the time of training.
    - Include passed count, not-passed count, and total unique number of trainers.

--- app/Lib/Reports/TrainingSlotCapacityReport.php
+++ app/Lib/Reports/TrainingSlotCapacityReport.php
@@ -2,13 +2,15 @@
 
 namespace App\Lib\Reports;
 
+use App\Models\Person;
+use App\Models\PersonStatus;
 use App\Models\Position;
 use Illuminate\Support\Facades\DB;
 
 class TrainingSlotCapacityReport
 {
 
-    /*
+    /**
      * Find the signups for all training shifts in a given year
      *
      * The method returns:
@@ -19,54 +21,124 @@ class TrainingSlotCapacityReport
      * max: sign up limit
      * signed_up: total signup count
      * filled: how full in a percentage (0 to 100+)
-     * alpha_count: alpha & prospective count
+     * pnv_count: alpha & prospective count
      * veteran_count: signups who are not alpha, auditor or prospective
      * auditor_count: signups who are auditors
      *
+     * @param $position
      * @param int $year the year to search
+     * @return array
      */
 
-    public static function execute($position, int $year)
+    public static function execute($position, int $year): array
     {
         $positionIds = [$position->id];
         if ($position->id == Position::HQ_FULL_TRAINING) {
             $positionIds[] = Position::HQ_REFRESHER_TRAINING;
         }
 
+        $trainerPositions = Position::TRAINERS[$position->id];
         $rows = DB::table('slot')
-            ->select(
-                'slot.id as slot_id',
-                'slot.description',
-                'slot.begins as date',
-                'slot.max',
-                DB::raw('(
-                SELECT COUNT(person.id) FROM person_slot
-                LEFT JOIN person ON person.id=person_slot.person_id
-                WHERE person_slot.slot_id=slot.id) as signed_up'),
-                DB::raw('(
-                SELECT COUNT(person.id) FROM person_slot
-                LEFT JOIN person ON person.id=person_slot.person_id AND person.status in ("alpha", "prospective")
-                WHERE person_slot.slot_id=slot.id) as alpha_count'),
-                DB::raw('(
-                SELECT COUNT(person.id) FROM person_slot
-                LEFT JOIN person ON person.id=person_slot.person_id AND person.status NOT IN ("alpha", "auditor", "prospective")
-                WHERE person_slot.slot_id=slot.id) as veteran_count'),
-                DB::raw('(
-                SELECT COUNT(person.id) FROM person_slot LEFT JOIN person ON person.id=person_slot.person_id AND person.status="auditor"
-                WHERE person_slot.slot_id=slot.id) as auditor_count')
-            )
+            ->select('id', 'description', 'begins', 'ends', 'max')
             ->whereYear('slot.begins', $year)
             ->whereIn('slot.position_id', $positionIds)
-            ->orderBy('slot.begins')->get();
+            ->where('active', 1)
+            ->orderBy('slot.begins')
+            ->get();
 
+        $trainerIds = [];
         foreach ($rows as $row) {
+            $ids = DB::table('person_slot')
+                ->where('slot_id', $row->id)
+                ->pluck('person_id');
+
+            $row->signed_up = $ids->count();
+
             if ($row->signed_up > 0 && $row->max > 0) {
                 $row->filled = round(($row->signed_up / $row->max) * 100);
             } else {
                 $row->filled = 0;
             }
+
+            if ($ids->isEmpty()) {
+                continue;
+            }
+
+            $statuses = PersonStatus::findStatusForIdsTime($ids->toArray(), $row->begins);
+            $alphas = 0;
+            $vets = 0;
+            $auditors = 0;
+
+            foreach ($ids as $personId) {
+                $status = $statuses->get($personId);
+                if ($status) {
+                    switch ($status->new_status) {
+                        case Person::AUDITOR:
+                            $auditors++;
+                            break;
+                        case Person::ALPHA:
+                        case Person::BONKED:
+                        case Person::PAST_PROSPECTIVE:
+                        case Person::PROSPECTIVE:
+                        case Person::PROSPECTIVE_WAITLIST:
+                        case Person::UBERBONKED:
+                            $alphas++;
+                            break;
+                        default:
+                            $vets++;
+                            break;
+                    }
+                } else {
+                    $vets++;
+                }
+            }
+            // Just in case the status records could not be found for everyone, dump
+            // the difference into the vets column
+            $vets += $ids->count() - $statuses->count();
+
+            $row->pnv_count = $alphas;
+            $row->veteran_count = $vets;
+            $row->auditor_count = $auditors;
+
+            $passed = DB::table('person_slot')
+                ->join('trainee_status', function ($j) use ($row) {
+                    $j->on('trainee_status.person_id', 'person_slot.person_id');
+                    $j->where('trainee_status.slot_id', $row->id);
+                })
+                ->where('person_slot.slot_id', $row->id)
+                ->where('trainee_status.passed', 1)
+                ->count();
+
+            $row->passed = $passed;
+            $row->not_passed = $ids->count() - $passed;
+
+            $trainerCount = 0;
+            foreach ($trainerPositions as $tid) {
+                // Find the trainer's slot that begins within a hour of the slot start time.
+                $trainerSlot = DB::table('slot')
+                    ->where('position_id', $tid)
+                    ->where('active', 1)
+                    ->whereRaw('begins BETWEEN DATE_SUB(?, INTERVAL 1 HOUR) AND ?', [$row->begins, $row->ends])
+                    ->where('description', $row->description)
+                    ->value('id');
+
+                if (!$trainerSlot) {
+                    continue;
+                }
+
+                $trainers = DB::table('person_slot')->where('slot_id', $trainerSlot)->get();
+                foreach ($trainers as $trainer) {
+                    $trainerIds[$trainer->person_id] = true;
+                }
+                $trainerCount += $trainers->count();
+            }
+
+            $row->trainer_count = $trainerCount;
         }
 
-        return $rows;
+        return [
+            'slots' => $rows,
+            'unique_trainers' => count(array_keys($trainerIds))
+        ];
     }
 }
