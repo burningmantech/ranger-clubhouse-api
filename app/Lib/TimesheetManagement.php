95eb3987b5121f71f29bb37d4bdd8221c8e4343b
mikeburg <mike@burg.me>
2023-01-07 14:50:05 -0800

    Start of TimesheetController cleanup. (#1356)
    - Moved some business logic into app/Lib/TimesheetManagement.
    - Unconfirm final confirmation when creating a new entry via signin or signout.

---
+++ app/Lib/TimesheetManagement.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Lib;
+
+use App\Models\Person;
+use App\Models\PersonEvent;
+use App\Models\PersonPosition;
+use App\Models\Position;
+use App\Models\Role;
+use App\Models\Timesheet;
+use App\Models\TimesheetLog;
+use App\Models\Training;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+
+class TimesheetManagement
+{
+    /**
+     * Unconfirm the entire timesheet
+     *
+     * @param Timesheet $timesheet
+     * @param string $reason
+     * @return void
+     */
+    public static function unconfirmTimesheet(Timesheet $timesheet, string $reason): void
+    {
+        $year = $timesheet->on_duty->year;
+
+        // Only unconfirm a timesheet if it's the current year.
+        if ($year != current_year()) {
+            return;
+        }
+
+        $event = PersonEvent::firstOrNewForPersonYear($timesheet->person_id, $year);
+        if (!$event->timesheet_confirmed) {
+            return;
+        }
+
+        $event->timesheet_confirmed = false;
+        $event->timesheet_confirmed_at = null;
+        $event->saveWithoutValidation();
+        $timesheet->log(TimesheetLog::UNCONFIRMED, "timesheet #{$timesheet->id} {$reason}");
+    }
+
+    /**
+     * Is the person allowed to work the given position?
+     *
+     * @param Person $person
+     * @param int $positionId
+     * @param int $requiredPositionId
+     * @param $response
+     * @param bool $signonForced
+     * @param $unqualifiedReason
+     * @return bool
+     */
+
+    public static function checkWorkAuthorization(Person $person,
+                                                  int    $positionId,
+                                                  int    &$requiredPositionId,
+                                                         &$response,
+                                                  bool   &$signonForced,
+                                                         &$unqualifiedReason): bool
+    {
+        $canForceSignon = Auth::user()?->hasRole([Role::ADMIN, Role::TIMESHEET_MANAGEMENT]);
+
+        $personId = $person->id;
+
+        // Confirm the person is allowed to sign in to the position
+        if (!PersonPosition::havePosition($personId, $positionId)) {
+            $response = ['status' => 'position-not-held'];
+            return false;
+        }
+
+        // Are they trained for this position?
+        if (!Training::isPersonTrained($person, $positionId, current_year(), $requiredPositionId)) {
+            $positionRequired = Position::retrieveTitle($requiredPositionId);
+            if ($canForceSignon) {
+                $signonForced = true;
+                $unqualifiedReason = Position::UNQUALIFIED_UNTRAINED;
+            } else {
+                $response = [
+                    'status' => 'not-trained',
+                    'position_title' => $positionRequired,
+                    'position_id' => $requiredPositionId
+                ];
+                return false;
+            }
+        }
+
+        // Sandman blocker - must be qualified
+        if ($positionId == Position::SANDMAN && !Position::isSandmanQualified($person, $unqualifiedReason)) {
+            if ($canForceSignon) {
+                $signonForced = true;
+            } else {
+                $response = [
+                    'status' => 'not-qualified',
+                    'unqualified_reason' => $unqualifiedReason,
+                    'unqualified_message' => Position::UNQUALIFIED_MESSAGES[$unqualifiedReason]
+                ];
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Return response for a timesheet sign-in or creation.
+     *
+     * @param string $action
+     * @param Timesheet $timesheet
+     * @param bool $signonForced
+     * @param int $requiredPositionId
+     * @param $unqualifiedReason
+     * @param $log
+     * @return JsonResponse
+     */
+
+    public static function reportSignIn(string    $action,
+                                        Timesheet $timesheet,
+                                        bool      $signonForced,
+                                        int       $requiredPositionId,
+                                                  $unqualifiedReason,
+                                                  $log): JsonResponse
+    {
+        $response = [
+            'status' => 'success',
+            'timesheet_id' => $timesheet->id,
+            'on_duty' => (string)$timesheet->on_duty,
+        ];
+
+        if ($signonForced) {
+            $response['forced'] = true;
+            $response['unqualified_reason'] = $unqualifiedReason;
+            $response['unqualified_message'] = Position::UNQUALIFIED_MESSAGES[$unqualifiedReason];
+            if ($requiredPositionId) {
+                $response['required_training'] = Position::retrieveTitle($requiredPositionId);
+            }
+
+            $log['forced'] = ['reason' => $unqualifiedReason];
+            if ($unqualifiedReason == Position::UNQUALIFIED_UNTRAINED) {
+                $log['forced']['position_id'] = $requiredPositionId;
+            }
+        }
+
+        $timesheet->log($action, $log);
+
+        return response()->json($response);
+    }
+}
