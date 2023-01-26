320d35c77906a0355408360fbc8b2f7575658395
mikeburg <mike@burg.me>
2023-01-25 18:16:54 -0800

    Tweaks to the Applicant Welcome Email. (VC Cadre request) (#1376)

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -18,7 +18,7 @@
         <div class="btn btn-primary" style="margin-top: 20px">
             <a href="{{$inviteUrl}}">Setup Your Clubhouse Account</a>
         </div>
-        <div style="color: #ff0000;margin-bottom: 25px">
+        <div style="color: #ff0000;margin-top: 25px;margin-bottom: 25px;">
             <b>The button will expire within 1 week.</b>
         </div>
         <p>
@@ -30,7 +30,7 @@
             When you successfully log in, you will see our dashboard outlining the
             remaining Milestones on the path to becoming a Black Rock Ranger. Click the link to
             "Review and Update Personal Information." Please make sure that your address, phone
-            number(s), and emergency contact info are all correct. Click the &ldquo;Update&rdquo;
+            number(s), and emergency contact info are all correct. Click the &ldquo;Save&rdquo;
             buttons on each page to save your changes.
         </p>
         <p>
@@ -39,18 +39,17 @@
         </p>
         <p>
             <b>2. Upload a BMID photo.</b><br>
-            Before you can sign up for training you&rsquo;ll need
-            to upload a photo for your Burning Man ID badge. <b>Before</b>
-            you select/take your photo, please read
-            <a href="https://docs.google.com/document/d/1hUqrJwdMG6XB5nk7Eh8nY2GH4uvAFTppmCKCmfidIvQ/edit">
+            Before you can sign up for training you'll need to upload a photo for your Burning Man ID badge.
+            <b>Before you select/take your photo</b>, please read
+            <a href="https://docs.google.com/document/d/1hUqrJwdMG6XB5nk7Eh8nY2GH4uvAFTppmCKCmfidIvQ">
                 the photo guidelines
             </a>.
         </p>
         <p>
             Log in to the Ranger Secret Clubhouse and click "Upload a New Photo" (on the
-            right side of the dashboard view on desktop, at the top on mobile). Follow the
-            instructions. Note that it may take 2-3 days for your BMID photo to be approved (they
-            can be pretty picky). If you encounter any problems, please email our fantastic BMID
+            right side of the dashboard view on desktop, at the top on mobile &amp; smaller screen tablets).
+            Follow the instructions. Note that it may take 2-3 days for your BMID photo to be approved
+            (the reviewers can be pretty picky). If you encounter any problems, please email our fantastic BMID
             experts at <a href="mailto:ranger-vc-list@burningman.org">ranger-vc-list@burningman.org</a>.
         </p>
         <p>
@@ -59,21 +58,21 @@
         </p>
         <p>
             <b>3. Read the Ranger Manual &amp; Complete the Online Course.</b><br>
-            Another thing you must do before you can sign up for training is to read the most recent version of the
-            Ranger Manual (links on <a
-                    href="https://ranger-clubhouse.burningman.org">ranger-clubhouse.burningman.org</a>
-            at the bottom on mobile, at the top and in the right sidebar on desktop). Once
-            you've read the manual, go to your Clubhouse dashboard, and click the link to take
-            the Online Course. This course is estimated to take 90 minutes-ish, and you can save
+            Another thing you must do before you can sign up for training is to
+            <a href="https://rangers.burningman.org/training/about-manual/">
+                read the most recent version of the Ranger Manual
+            </a>.
+            Once you've read the manual, sign back into the Clubhouse and click the "Go To Online Course" button under
+            the "Read the Ranger Manual & Complete The Online Course" dashboard step. This course is estimated to take
+            90 minutes-ish, and you can save
             your progress and return at any time.
         </p>
         <p>
             <b>4. Sign up for In-Person Training.</b><br>
-            Once you have completed the other steps,
-            Sign up for In-Person Training, in the Clubhouse. Locations and dates for In-Person
-            Trainings will be available in the Clubhouse. Rangers train every year, so you will need
-            to go to a training this year, even if you went to one last year. Please ensure you have
-            signed up by June 15th.
+            Once you have completed the other steps, sign up for an In-Person Training, in the Clubhouse.
+            Locations and dates for In-Person Trainings will be available in the Clubhouse. Rangers train
+            every year, so you will need to go to a training this year, even if you went to
+            one last year. Please ensure you have signed up by June 15th.
         </p>
         <p>
             The general locations of Ranger Trainings are posted at this link:
@@ -81,9 +80,9 @@
             which will continue to be updated through the end of May, as
             new trainings are announced. Specific location information will be released by email
             closer to the training date to those Rangers signed up for that date. Stay tuned to
-            Ranger Applicant News as new trainings are posted Please sign up for a training by June
-            15th, as they fill up. There are no walk-ins for trainings, so make sure you sign
-            up in the Clubhouse.
+            Ranger Applicant News as new trainings are posted. Please sign up for a training by June
+            15th, as they fill up.  <b style="color: #ff0000;">There are no walk-ins for trainings</b>,
+            so make sure you sign up in the Clubhouse.
         </p>
         <p>
             <b>In order to proceed to Milestone 4: Walk your Alpha shift, you will
