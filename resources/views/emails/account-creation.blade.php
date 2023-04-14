30dbcbba623f6ec1d49eccf111b3c32b4009649d
mikeburg <mike@burg.me>
2023-04-14 01:47:46 -0700

    Framework upgrades. (#1443)

--- resources/views/emails/account-creation.blade.php
+++ resources/views/emails/account-creation.blade.php
@@ -1,15 +1,16 @@
 <x-html-email>
-<p>
-@if ($status == 'failed')
-Failed to create an account ({{$details}}) for:
-@else
-Successfully created an account for: {{$person->callsign}}
-@endif
-{{$person->first_name}} {{$person->last_name}} <a href="mailto:{{$person->email}}">{{$person->email}}</a> who intends to {{$intent}}
-</p>
-<p>
-Your humble servant,<br>
-<br>
-The Clubhouse
-</p>
+    <p>
+        @if ($status == 'failed')
+            Failed to create an account ({{$details}}) for:
+        @else
+            Successfully created an account for: {{$person->callsign}}
+        @endif
+        {{$person->first_name}} {{$person->last_name}} <a href="mailto:{{$person->email}}">{{$person->email}}</a> who
+        intends to {{$intent}}
+    </p>
+    <p>
+        Your humble servant,<br>
+        <br>
+        The Clubhouse
+    </p>
 </x-html-email>
