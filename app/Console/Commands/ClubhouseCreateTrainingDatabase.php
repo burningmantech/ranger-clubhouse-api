dee392bcb29fd57fa6e8f053bc560e1741a1061a
mikeburg <mike@burg.me>
2023-06-05 19:52:27 -0700

    More ghd server speed ups. (#1497)

--- app/Console/Commands/ClubhouseCreateTrainingDatabase.php
+++ app/Console/Commands/ClubhouseCreateTrainingDatabase.php
@@ -28,7 +28,7 @@ class ClubhouseCreateTrainingDatabase extends Command
      *
      * @return int
      */
-    public function handle() : int
+    public function handle(): int
     {
         $ghdVar = GroundHogDay::ENVIRONMENT_VAR;
         $groundHogDay = env($ghdVar);
@@ -50,20 +50,36 @@ class ClubhouseCreateTrainingDatabase extends Command
 
         $this->info("Dumping from $cloneDb");
 
-        if (shell_exec("mysqldump -h $cloneHost -u $cloneUser --add-drop-table --single-transaction --skip-add-locks --quick  --ignore-table=$cloneDb.log --ignore-table=$cloneDb.mail_log --ignore-table=$cloneDb.action_logs > $cloneDump")) {
+        file_put_contents($cloneDump, "SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, AUTOCOMMIT = 0;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS = 0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0;
+");
+        $this->info("mysqldump -h $cloneHost -u $cloneUser --add-drop-table --single-transaction --skip-add-locks --quick --ignore-table=$cloneDb.log --ignore-table=$cloneDb.mail_log --ignore-table=$cloneDb.action_logs >> $cloneDump");
+
+        if (shell_exec("mysqldump -h $cloneHost -u $cloneUser --add-drop-table --single-transaction --skip-add-locks --quick  --ignore-table=$cloneDb.person_message --ignore-table=$cloneDb.log --ignore-table=$cloneDb.mail_log --ignore-table=$cloneDb.action_logs $cloneDb >> $cloneDump")) {
             $this->error("Cannot dump the ignore database.");
             return true;
         }
 
-        if (shell_exec("mysqldump -h $cloneHost -u $cloneUser --add-drop-table --no-data $cloneDb log mail_log >> $cloneDump")) {
+        $this->info("Dumping partial schema");
+
+        if (shell_exec("mysqldump -h $cloneHost -u $cloneUser --add-drop-table --no-data $cloneDb log mail_log person_message >> $cloneDump")) {
             $this->error("Cannot dump the database structure for selected tables.");
             return true;
         }
 
+        $this->info("Dumping selected action log events");
         if (shell_exec("mysqldump -h $cloneHost -u $cloneUser   --single-transaction --skip-add-locks --quick --where=\"event in ('person-slot-add', 'person-slot-remove')\" $cloneDb action_logs  >> $cloneDump")) {
             $this->error("Cannot dump the database structure for selected tables.");
             return true;
         }
+
+        file_put_contents($cloneDump, "
+        SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+SET AUTOCOMMIT = @OLD_AUTOCOMMIT;
+COMMIT;
+", FILE_APPEND);
 
         // Load database up here.
         $user = config('database.connections.mysql.username');
@@ -72,7 +88,7 @@ class ClubhouseCreateTrainingDatabase extends Command
         $host = config('database.connections.mysql.host');
         putenv("MYSQL_PWD=$pwd");
         $this->info("Loading into $db");
-        if (shell_exec("mysql -h $host -u $user $db < dump-tmp.sql")) {
+        if (shell_exec("mysql -h $host -u $user $db < $cloneDump")) {
             $this->error("Could not load database from production server");
             unlink($cloneDump);
             return 1;
