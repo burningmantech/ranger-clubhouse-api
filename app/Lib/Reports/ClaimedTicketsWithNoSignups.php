8e0d9a933c14c143bab9bfb7b1fe98cbf2ee4d1c
mikeburg <mike@burg.me>
2023-07-25 20:54:35 -0700

    Include training status on Ticket Claimed with no Signups report.

--- app/Lib/Reports/ClaimedTicketsWithNoSignups.php
+++ app/Lib/Reports/ClaimedTicketsWithNoSignups.php
@@ -4,6 +4,8 @@ namespace App\Lib\Reports;
 
 use App\Models\AccessDocument;
 use App\Models\Position;
+use App\Models\Slot;
+use App\Models\TrainerStatus;
 use Illuminate\Support\Facades\DB;
 
 class ClaimedTicketsWithNoSignups
@@ -48,7 +50,33 @@ class ClaimedTicketsWithNoSignups
             ->get()
             ->keyBy('person_id');
 
-        $tickets = [];
+        $trainings = Slot::select('slot.*', 'person_slot.person_id', 'trainee_status.passed')
+            ->join('person_slot', 'slot.id', 'person_slot.slot_id')
+            ->leftJoin('trainee_status', function ($j) {
+                $j->on('trainee_status.slot_id', 'slot.id');
+                $j->whereColumn('trainee_status.person_id', 'person_slot.person_id');
+            })
+            ->where('slot.active', true)
+            ->where('slot.position_id', Position::TRAINING)
+            ->where('slot.begins_year', current_year())
+            ->whereIntegerInRaw('person_slot.person_id', $peopleIds)
+            ->get()
+            ->groupBy('person_id');
+
+        $trainers = Slot::select('slot.*', 'person_slot.person_id', 'trainer_status.status as trainer_status')
+            ->join('person_slot', 'slot.id', 'person_slot.slot_id')
+            ->leftJoin('trainer_status', function ($j) {
+                $j->on('trainer_status.trainer_slot_id', 'slot.id');
+                $j->whereColumn('trainer_status.person_id', 'person_slot.person_id');
+            })
+            ->where('slot.active', true)
+            ->where('slot.position_id', Position::TRAINER)
+            ->where('slot.begins_year', current_year())
+            ->whereIntegerInRaw('person_slot.person_id', $peopleIds)
+            ->get()
+            ->groupBy('person_id');
+
+        $people = [];
         foreach ($claimedRows as $claimed) {
             $person = $claimed->person;
             $personId = $person->id;
@@ -61,7 +89,31 @@ class ClaimedTicketsWithNoSignups
                 continue;
             }
 
-            $tickets[] = [
+            $trained = $trainings->get($personId);
+            $trainer = $trainers->get($personId);
+
+            $didTrain = false;
+            $didTeach = false;
+
+            if ($trained) {
+                foreach ($trained as $slot) {
+                    if ($slot->passed) {
+                        $didTrain = true;
+                        break;
+                    }
+                }
+            }
+
+            if ($trainer) {
+                foreach ($trainer as $slot) {
+                    if ($slot->trainer_status == TrainerStatus::ATTENDED) {
+                        $didTeach = true;
+                        break;
+                    }
+                }
+            }
+
+            $people[] = [
                 'id' => $personId,
                 'callsign' => $person->callsign,
                 'status' => $person->status,
@@ -73,10 +125,23 @@ class ClaimedTicketsWithNoSignups
                 'ot_completed' => $otCompleted,
                 'has_signups' => $hasSignups,
                 'did_work' => $didWork,
+                'have_trained' => ($didTrain || $didTeach),
+                'did_teach' => $didTeach,
+                'trainings' => $trained?->map(fn($t) => [
+                    'id' => $t->id,
+                    'begins' => (string)$t->begins,
+                    'status' => ($t->has_ended ? ($t->passed ? 'passed' : 'not-passed') : 'pending')
+                ]
+                )->values()->toArray(),
+                'teachings' => $trainer?->map(fn($t) => [
+                    'id' => $t->id,
+                    'begins' => (string)$t->begins,
+                    'status' => ($t->has_ended ? $t->trainer_status : 'pending')]
+                )->values()->toArray(),
             ];
         }
 
-        usort($tickets, fn($a, $b) => strcasecmp($a['callsign'], $b['callsign']));
-        return $tickets;
+        usort($people, fn($a, $b) => strcasecmp($a['callsign'], $b['callsign']));
+        return $people;
     }
 }
