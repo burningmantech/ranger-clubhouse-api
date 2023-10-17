d5e7bbf33a0b7fd0c0a9e7b73395ae54c50a3699
mikeburg <mike@burg.me>
2023-10-17 13:06:45 -0700

    Reworked unconfirmed timesheet report to avoid raw sql. (#1606)

--- app/Lib/Reports/PeopleWithUnconfirmedTimesheetsReport.php
+++ app/Lib/Reports/PeopleWithUnconfirmedTimesheetsReport.php
@@ -2,6 +2,8 @@
 
 namespace App\Lib\Reports;
 
+use App\Models\Person;
+use App\Models\Timesheet;
 use Illuminate\Support\Facades\DB;
 
 class PeopleWithUnconfirmedTimesheetsReport
@@ -10,21 +12,45 @@ class PeopleWithUnconfirmedTimesheetsReport
      * Retrieve all people who has not indicated their timesheet entries are correct.
      */
 
-    public static function execute(int $year)
+    public static function execute(int $year): array
     {
-        return DB::select(
-            "SELECT person.id, callsign, first_name, last_name, email, home_phone,
-                    (SELECT count(*) FROM timesheet
-                        WHERE person.id=timesheet.person_id
-                          AND YEAR(timesheet.on_duty)=?
-                          AND  timesheet.review_status not in ('verified', 'rejected')  ) as unverified_count
-               FROM person
-               LEFT JOIN person_event ON person_event.person_id=person.id AND person_event.year=?
-               WHERE status in ('active', 'inactive', 'inactive extension', 'retired')
-                 AND IFNULL(person_event.timesheet_confirmed, FALSE) != TRUE
-                 AND EXISTS (SELECT 1 FROM timesheet WHERE timesheet.person_id=person.id AND YEAR(timesheet.on_duty)=?)
-               ORDER BY callsign",
-            [$year, $year, $year]
-        );
+        $rows = Person::select(
+            'id', 'callsign', 'first_name', 'last_name', 'email', 'home_phone',
+        )->addSelect([
+            'unverified_count' => function ($q) use ($year) {
+                $q->from('timesheet')
+                    ->selectRaw('count(*)')
+                    ->whereColumn('person.id', 'timesheet.person_id')
+                    ->whereYear('on_duty', $year)
+                    ->whereIn('timesheet.review_status', [Timesheet::STATUS_UNVERIFIED, Timesheet::STATUS_REJECTED]);
+            }
+        ])->leftJoin('person_event', function ($j) use ($year) {
+            $j->on('person_event.person_id', 'person.id');
+            $j->where('person_event.year', $year);
+        })->whereIn('person.status', Person::ACTIVE_STATUSES)
+            ->whereRaw('IFNULL(person_event.timesheet_confirmed, FALSE) != TRUE')
+            ->whereExists(function ($sql) use ($year) {
+                $sql->from('timesheet')
+                    ->select(DB::raw(1))
+                    ->whereColumn('person.id', 'timesheet.person_id')
+                    ->whereYear('on_duty', $year)
+                    ->limit(1);
+            })->orderBy('callsign')
+            ->get();
+
+        $people = [];
+        foreach ($rows as $person) {
+            $people[] = [
+                'id' => $person->id,
+                'callsign' => $person->callsign,
+                'first_name' => $person->first_name,
+                'last_name' => $person->last_name,
+                'email' => $person->email,
+                'home_phone' => $person->home_phone,
+                'unverified_count' => $person->unverified_count,
+            ];
+        }
+
+        return $people;
     }
 }
