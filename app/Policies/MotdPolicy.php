ed7872566b7f2c11aad738cb76203bd8c4d7fdea
mikeburg <mike@burg.me>
2023-11-12 09:35:54 -0800

    New permission Announcement Management for the MOTD. (#1628)
    * New permission Announcement Management for the MOTDs.
    
    * Missed file.

--- app/Policies/MotdPolicy.php
+++ app/Policies/MotdPolicy.php
@@ -3,46 +3,48 @@
 namespace App\Policies;
 
 use App\Models\Person;
-use App\Models\Setting;
 use App\Models\Role;
-
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class MotdPolicy
 {
     use HandlesAuthorization;
 
-    public function before(Person $user) {
-        if ($user->hasRole([ Role::ADMIN, Role::MEGAPHONE ])) {
+    public function before(Person $user): ?true
+    {
+        if ($user->hasRole([Role::ADMIN, Role::ANNOUNCEMENT_MANAGEMENT])) {
             return true;
         }
+
+        return null;
     }
 
-    public function index(Person $user)
+    public function index(Person $user): false
     {
         return false;
     }
 
-    public function bulletin(Person $user) {
+    public function bulletin(Person $user): true
+    {
         return true;
     }
 
-    public function show(Person $user, Motd $motd)
+    public function show(Person $user, Motd $motd): false
     {
         return false;
     }
 
-    public function create(Person $user)
+    public function create(Person $user): false
     {
         return false;
     }
 
-    public function update(Person $user, Motd $motd)
+    public function update(Person $user, Motd $motd): false
     {
         return false;
     }
 
-    public function destroy(Person $user, Motd $motd)
+    public function destroy(Person $user, Motd $motd): false
     {
         return false;
     }
