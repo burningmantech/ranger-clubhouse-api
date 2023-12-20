96175ead1fa846271a61e3ce22a14f72d263693e
mikeburg <mike@burg.me>
2023-12-19 19:41:50 -0800

    Put back route binds for special models. (#1672)

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\Bmid;
+use App\Models\Document;
 use App\Models\Help;
 use App\Models\PersonEvent;
+use App\Models\Setting;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
 
@@ -27,6 +30,14 @@ class RouteServiceProvider extends ServiceProvider
     {
         parent::boot();
 
+        Route::bind('bmid', function ($id) {
+            return Bmid::find($id) ?? abort(404);
+        });
+
+        Route::bind('document', function ($id) {
+            return Document::findIdOrTag($id) ?? abort(404);
+        });
+
         Route::bind('help', function ($id) {
             return Help::findByIdOrSlug($id) ?? abort(404);
         });
@@ -34,6 +45,11 @@ class RouteServiceProvider extends ServiceProvider
         Route::bind('person-event', function ($id) {
             return PersonEvent::findForRoute($id) ?? abort(404);
         });
+
+        Route::bind('setting', function ($id) {
+            return Setting::find($id) ?? abort(404);
+        });
+
 
         $this->routes(function () {
             Route::prefix('')
