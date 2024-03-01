13ffd66e6e8d78d5d8e675816287ac5e8a40d283
mikeburg <mike@burg.me>
2024-02-29 17:06:58 -0800

    For development: replaced Clockwork with Laravel Telescope. (#1705)
    * For development: replaced Clockwork with Laravel Telescope.
    
    * Move Laravel Telescope from dev to required in composer.lock yet keep it disabled.

---
+++ app/Providers/TelescopeServiceProvider.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Person;
+use App\Models\Role;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Telescope\IncomingEntry;
+use Laravel\Telescope\Telescope;
+use Laravel\Telescope\TelescopeApplicationServiceProvider;
+
+class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        // Telescope::night();
+
+        $this->hideSensitiveRequestDetails();
+
+        $isLocal = $this->app->environment('local');
+
+        Telescope::filter(function (IncomingEntry $entry) use ($isLocal) {
+            return $isLocal ||
+                   $entry->isReportableException() ||
+                   $entry->isFailedRequest() ||
+                   $entry->isFailedJob() ||
+                   $entry->isScheduledTask() ||
+                   $entry->hasMonitoredTag();
+        });
+    }
+
+    /**
+     * Prevent sensitive request details from being logged by Telescope.
+     */
+    protected function hideSensitiveRequestDetails(): void
+    {
+        if ($this->app->environment('local')) {
+            return;
+        }
+
+        Telescope::hideRequestParameters(['_token']);
+
+        Telescope::hideRequestHeaders([
+            'cookie',
+            'x-csrf-token',
+            'x-xsrf-token',
+        ]);
+    }
+
+    /**
+     * Register the Telescope gate.
+     *
+     * This gate determines who can access Telescope in non-local environments.
+     */
+    protected function gate(): void
+    {
+        // Not exactly necessary since Telescope is only used in development environments,
+        // but ya never know.
+        Gate::define('viewTelescope', fn (Person $user) => $user->hasRole(Role::TECH_NINJA));
+    }
+}
