15cc5eadfd55b41dd04ac30f581e874b1484b5c8
mikeburg <frankenstein@burningman.org>
2024-11-07 20:58:33 -0800

    Callsign assignment - Convert Microsoft special characters to regular characters. (#1900)
    E.g., right apostrophe -> single quote.

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -1395,8 +1395,47 @@ class Person extends ApiModel implements AuthenticatableContract, AuthorizableCo
     public function setCallsignAttribute(string $value): void
     {
         $value = trim($value);
+        // Convert microsoft special formatting to regular characters
+        $search = [
+            "\xC2\xAB",     // « (U+00AB) in UTF-8
+            "\xC2\xBB",     // » (U+00BB) in UTF-8
+            "\xE2\x80\x98", // ‘ (U+2018) in UTF-8
+            "\xE2\x80\x99", // ’ (U+2019) in UTF-8
+            "\xE2\x80\x9A", // ‚ (U+201A) in UTF-8
+            "\xE2\x80\x9B", // ‛ (U+201B) in UTF-8
+            "\xE2\x80\x9C", // “ (U+201C) in UTF-8
+            "\xE2\x80\x9D", // ” (U+201D) in UTF-8
+            "\xE2\x80\x9E", // „ (U+201E) in UTF-8
+            "\xE2\x80\x9F", // ‟ (U+201F) in UTF-8
+            "\xE2\x80\xB9", // ‹ (U+2039) in UTF-8
+            "\xE2\x80\xBA", // › (U+203A) in UTF-8
+            "\xE2\x80\x93", // – (U+2013) in UTF-8
+            "\xE2\x80\x94", // — (U+2014) in UTF-8
+            "\xE2\x80\xA6"  // … (U+2026) in UTF-8
+        ];
+
+        $replacements = [
+            "<<",
+            ">>",
+            "'",
+            "'",
+            "'",
+            "'",
+            '"',
+            '"',
+            '"',
+            '"',
+            "<",
+            ">",
+            "-",
+            "-",
+            "..."
+        ];
+
+        $value = str_replace($search, $replacements, $value);
+
         $this->attributes['callsign'] = $value;
-        $this->attributes['callsign_normalized'] = self::normalizeCallsign($value ?? ' ');
+        $this->attributes['callsign_normalized'] = self::normalizeCallsign($value);
         $this->attributes['callsign_soundex'] = metaphone(self::spellOutNumbers($this->attributes['callsign_normalized']));
     }
 
